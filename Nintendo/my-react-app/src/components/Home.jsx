b8a6ebd7f7c12760b844872c22718040f5eb8e58
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-06 13:10:58 -0400

    export function Home

--- Nintendo/my-react-app/src/components/Home.jsx
+++ Nintendo/my-react-app/src/components/Home.jsx
@@ -1,1 +1,11 @@
 export default function Home
+() {
+  return (
+    <div className="intro-container">
+        <div className="intro">
+      <h1>Welcome to Mario's worlds!</h1>
+      <p>On this site you can explore some of Mario's friends, as well as the many different outfits of Mario. Happy clicking!</p>
+      </div>
+    </div>
+  )
+}
