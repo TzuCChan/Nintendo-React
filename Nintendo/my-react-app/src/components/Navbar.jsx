ca6a957c3eef60457cb9a1d715738d25129a857b
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-06 13:02:16 -0400

    export function Navbar

--- Nintendo/my-react-app/src/components/Navbar.jsx
+++ Nintendo/my-react-app/src/components/Navbar.jsx
@@ -1,3 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './characters.css'
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <h1 className='title'>Mario's World</h1>
+        <hr></hr>
+        <nav className='btn-container'>
+          <Link to="/"><button className='nav-btn'>Home</button></Link>
+          <Link to="/characters"><button className='nav-btn'>Super Mario</button></Link>
+        </nav>
+      </header>
+    </div>
+      )
+    
+}
