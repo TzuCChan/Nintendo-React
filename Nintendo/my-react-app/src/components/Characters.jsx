e7cadafbe85c66aa05ff78818523a3cccf17e233
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-06 13:42:37 -0400

    export function Characters

--- Nintendo/my-react-app/src/components/Characters.jsx
+++ Nintendo/my-react-app/src/components/Characters.jsx
@@ -1,3 +1,7 @@
 import {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './characters.css'
+
+export default function Characters({ data }) {
+  const [uniqueCharacters, setUniqueCharacters] = useState([])
+}
