f27d6d22b09a40b528a0a48dfbe5bfa06d3b2565
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-06 13:57:01 -0400

    useEffect

--- Nintendo/my-react-app/src/components/Characters.jsx
+++ Nintendo/my-react-app/src/components/Characters.jsx
@@ -4,4 +4,10 @@ import './characters.css'
 
 export default function Characters({ data }) {
   const [uniqueCharacters, setUniqueCharacters] = useState([])
+
+useEffect(() => {
+    let uniqueCharacterArray = [...new Map(data.map((char) => [char.character, char])).values()];
+    console.log(uniqueCharacterArray)
+    setUniqueCharacters(uniqueCharacterArray)    
+  }, [data])
 }
