7fc85271647972bac84f79587e5f735377c6556d
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-10 13:59:09 -0400

    export default Character

--- Nintendo/my-react-app/src/components/Character.jsx
+++ Nintendo/my-react-app/src/components/Character.jsx
@@ -1,3 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import './characters.css'
+
+export default function Character({ data }) {
+  const [filteredData, setFilteredData] = useState([])
+  let { name } = useParams()
+}
