546c177d22700bba5fc0e89ecb6c9bb6d7b00b7e
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-10-24 12:40:12 -0400

    index.js

--- Nintendo/my-react-app/src/index.js
+++ Nintendo/my-react-app/src/index.js
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
